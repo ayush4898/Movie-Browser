71fc3b10ef572f48894877ed6c77d18ce5b3751d
ayush goel <ayush4898@gmail.com>
2021-04-15 12:00:02 +0530

    added first middleware i.e. logger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore , applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './component/App';
 import  rootReducer  from './reducers'
 
-const store = createStore(rootReducer);
+// middleware is placed between dispatch and reducer 
+let logger = store => next => (action) => {
+    console.log('action.type :',action.type);
+    next(action);
+}
+    
+
+
+const store = createStore(rootReducer,applyMiddleware(logger));
 // console.log('STORE', store);
 
 ReactDOM.render(
